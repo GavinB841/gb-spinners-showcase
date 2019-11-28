628ca84f56d09eeb543360a2631944655b50a1e0
Gavin Boyle <gboyle@nightline-delivers.com>
2019-11-28 11:09:01 +0000

    Spinner library files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { GbSpinnerService } from '@gavinb841/gb-spinner';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'gb-spinners-showcase';
+  constructor(private gbSpinner: GbSpinnerService) {
+
+  }
+
+  wrapSpinner() {
+    this.gbSpinner.show();
+    setTimeout(() => {
+      this.gbSpinner.hide();
+    }, 2000);
+  }
 }
